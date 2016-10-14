9a7874b6762263f829d8583a71698053b87df2a1
Jivan Amara <Development@JivanAmara.net>
2016-10-14 16:52:45 -0700

    Api server now at www.mandarintt.com/mobile-api/ so the ssl cert for that domain can be used. Random syllable collection & tone checks ensure they're authenticated before transmitting.

--- www/js/index.js
+++ www/js/index.js
@@ -52,7 +52,6 @@ function ResultHandler(
     rh.setAudioUrl = function(audioUrl) {
         rh.attemptAudioElem.attr('src', audioUrl);
         rh.attemptAudioElem[0].load();
-        rh.attemptAudioElem[0].play();
     }
     
     rh.spinner = function() {
@@ -111,21 +110,33 @@ function ToneTutorServices(promptHandler, resultHandler) {
     var tts = this;
     tts.resultHandler = resultHandler;
     tts.promptHandler = promptHandler;
-    
-    tts.host = 'http://192.168.1.100:8001';
-    tts.getSyllableUrl = tts.host + '/api/randomsyllable/';
+
+//    tts.host = 'http://test-api.mandarintt.com';
+//    tts.host = 'http://192.168.1.100:9001';
+    tts.host = 'https://www.mandarintt.com';
+    tts.getSyllableUrl = tts.host + '/mobile-api/randomsyllable/';
     tts.getSyllableTimeout = 6000;
-    tts.checkSyllableUrl = tts.host + '/api/tonecheck/';
+    tts.checkSyllableUrl = tts.host + '/mobile-api/tonecheck/';
     tts.checkSyllableTimeout = 40000;
     tts.currentSyllable = {
         'sound': null, 'tone': null, 'display': null, 'url': null, 'hanzi': null
     };
-    tts.authUrl = tts.host + '/api/tokenauth/';
+    tts.authUrl = tts.host + '/mobile-api/tokenauth/';
     tts.authTimeout = 6000;
     tts.authToken = '';
 
-    tts.authenticate = function(success) {
+    tts.authenticate = function(success, error) {
         console.log('authenticate() entered');
+        if (error === undefined) {
+            console.log('No error callback provided, using default.');
+            error = function(jqXHR, textStatus, errorThrown) {
+                console.log('authenticate failed: ' + textStatus + ', ' + errorThrown);
+            }
+        }
+        if (success === undefined) {
+            console.log('No success callback provided, will only collect auth token.')
+            success = function(){};
+        }
         $.ajax({
             url: tts.authUrl,
             timeout: tts.authTimeout,
@@ -135,106 +146,133 @@ function ToneTutorServices(promptHandler, resultHandler) {
             success: function(data, textStatus, jqXHR) {
                 console.log('authenticate successful');
                 tts.authToken = data.auth_token;
-                if (success != undefined) {
-                    success();
-                }
+                success();
             },
-            error: function(jqXHR, textStatus, errorThrown) {
-                console.log('authenticate failed: ' + textStatus + ', ' + errorThrown);
-            }
-        })
+            error: error,
+        });
+        console.log('leaving authenticate()');
     };
 
-    this.getRandomSyllable = function() {
+    this.ensureAuthenticated = function(afterAuthentication) {
+        /* afterAuthentication is a callback which takes no arguments & is only called
+         * if authentication has already been performed or after successful authentication.
+         */
+        if (tts.authToken == '') {
+            // Looks like we need to authenticate.
+            tts.authenticate(afterAuthentication);
+        }
+        else {
+            // Looks like we're already authenticated, continue
+            afterAuthentication();
+        }
+    }
+    
+    this._getRandomSyllable = function() {
         /* Collects a new syllable, updates the src attribute of element with id 'self.exampleAudioId'.
          * Fills in the innerHTML of elements with class self.syllableTextClass with the new syllable.
         */
-        tts.resultHandler.hide();
-        $.ajax({
-            type: 'GET',
-            url: tts.getSyllableUrl,
-            timeout: tts.getSyllableTimeout,
-            dataType: 'json',
-            headers: {Authorization: 'Token ' + tts.authToken},
+        var f = function() {
+            tts.resultHandler.hide();
+            $.ajax({
+                type: 'GET',
+                url: tts.getSyllableUrl,
+                timeout: tts.getSyllableTimeout,
+                dataType: 'json',
+                headers: {Authorization: 'Token ' + tts.authToken},
 
-            success: function(data, textStatus, jqXHR) {
-                console.log('remote call in getRandomSyllable() successful');
-                tts.currentSyllable.tone = data.tone;
-                tts.currentSyllable.sound = data.sound;
-                tts.currentSyllable.display = data.display;
-                tts.currentSyllable.url = tts.host + data.url;
-                tts.currentSyllable.hanzi = data.hanzi;
+                success: function(data, textStatus, jqXHR) {
+                    console.log('remote call in getRandomSyllable() successful');
+                    tts.currentSyllable.tone = data.tone;
+                    tts.currentSyllable.sound = data.sound;
+                    tts.currentSyllable.display = data.display;
+                    tts.currentSyllable.url = tts.host + data.url;
+                    tts.currentSyllable.hanzi = data.hanzi;
 
-                // --- Generate the string to prompt the user with.
-                var hanziList = '';
-                for (var i = 0; i < tts.currentSyllable.hanzi.length; i++) {
-                    hanziList = hanziList + tts.currentSyllable.hanzi[i][0];
-                    if (i < tts.currentSyllable.hanzi.length - 1) {
-                        hanziList = hanziList + ', '
+                    // --- Generate the string to prompt the user with.
+                    var hanziList = '';
+                    for (var i = 0; i < tts.currentSyllable.hanzi.length; i++) {
+                        hanziList = hanziList + tts.currentSyllable.hanzi[i][0];
+                        if (i < tts.currentSyllable.hanzi.length - 1) {
+                            hanziList = hanziList + ', '
+                        }
                     }
-                }
-                var promptText = data.display;
-                promptText = promptText + ' (' + hanziList + ')';
+                    var promptText = data.display;
+                    promptText = promptText + ' (' + hanziList + ')';
+                    tts.promptHandler.prompt(promptText, tts.currentSyllable.url);
+                    return this;
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    console.log('remote call in getRandomSyllable() failed: ' + textStatus);
+                    console.log(errorThrown);
+                    return this;
+                },
+            });
+        }
+        return f;
+    }
 
-                tts.promptHandler.prompt(promptText, tts.currentSyllable.url);
-                return this;
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-                console.log('remote call in getRandomSyllable() failed: ' + textStatus);
-                console.log(errorThrown);
-                return this;
-            },
-        });
-    };
+    this.getRandomSyllable = function() {
+        // This is just a wrapper function to make sure the user is authenticated before
+        // attempting to get a random syllable.
+        tts.ensureAuthenticated(tts._getRandomSyllable());
+    }
 
     this.checkSyllable = function(attemptData, attemptMd5) {
         /* attemptData is a Blob containing the user's attempt at pronunciation.
-         * 
+         * attemptMd5 is the MD5 Sum of attemptData.
+         * Returns a function that takes no arguments but has access to attemptData & attemptMd5
+         *  for use in a callback.
          */
-        console.log('checkSyllableFile: entered');
-        console.log('attempt md5: ' + attemptMd5);
-        var data = new FormData();
-        data.append('attempt', attemptData);
-        data.append('attempt_md5', attemptMd5);
-        data.append('extension', '3gpp');
-        data.append('expected_sound', tts.currentSyllable.sound);
-        data.append('expected_tone', tts.currentSyllable.tone);
-        data.append('is_native', 'false');
-
-        $.ajax({
-            type: 'POST',
-            url: tts.checkSyllableUrl,
-            timeout: tts.checkSyllableTimeout,
-            data: data,
-            processData: false,
-            contentType: false,
-            headers: {Authorization: 'Token ' + this.authToken},
-            dataType: 'json',
-            success: function(data, textStatus, jqXHR) {
-                console.log('checkSyllable ajax success:');
-                console.log(data);
-
-                if (data.tone == null) {
-                    tts.resultHandler.unknown();
-                    tts.resultHandler.show();
+        console.log('checkSyllable() entered with params: ' + attemptData.length + ' ' + attemptMd5);
+        var f = function() {
+            console.log('checkSyllableFile: entered');
+            console.log('attempt md5: ' + attemptMd5);
+            var data = new FormData();
+            data.append('attempt', attemptData);
+            data.append('attempt_md5', attemptMd5);
+            data.append('extension', '3gpp');
+            data.append('expected_sound', tts.currentSyllable.sound);
+            data.append('expected_tone', tts.currentSyllable.tone);
+            data.append('is_native', 'false');
+    
+            $.ajax({
+                type: 'POST',
+                url: tts.checkSyllableUrl,
+                timeout: tts.checkSyllableTimeout,
+                data: data,
+                processData: false,
+                contentType: false,
+                headers: {Authorization: 'Token ' + this.authToken},
+                dataType: 'json',
+                success: function(data, textStatus, jqXHR) {
+                    console.log('checkSyllable ajax success:');
+                    console.log(data);
+                    var attemptUrl = data.attempt_url
+                    tts.resultHandler.setAudioUrl(attemptUrl)
+                    if (data.tone == null) {
+                        tts.resultHandler.unknown();
+                        tts.resultHandler.show();
+                    }
+                    else if (data.tone == tts.currentSyllable.tone) {
+                        tts.resultHandler.success();
+                        tts.resultHandler.show();
+                    }
+                    else {
+                        tts.resultHandler.failure(data.tone);
+                        tts.resultHandler.show();
+                    }
+                },
+                error: function(jqXHR, textStatus, errorThrown) {
+                    console.log('checkSyllable ajax failure: ');
+                    console.log('textStatus, errorThrown: ' + textStatus + ', ' + errorThrown);
+                    tts.resultHandler.hide();
                 }
-                else if (data.tone == tts.currentSyllable.tone) {
-                    tts.resultHandler.success();
-                    tts.resultHandler.show();
-                }
-                else {
-                    tts.resultHandler.failure(data.tone);
-                    tts.resultHandler.show();
-                }
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-                console.log('checkSyllable ajax failure: ');
-                console.log('textStatus, errorThrown: ' + textStatus + ', ' + errorThrown);
-                tts.resultHandler.hide();
-            }
-        });
-        console.log('checkSyllableFile: leaving')
+            });
+            console.log('checkSyllable: leaving');
+        }
+        return f;
     }
+    
     return tts;
 };
 
@@ -242,7 +280,7 @@ var app = {
     // Application Constructor
     initialize: function(toneTutorServices) {
         this.toneTutorServices = toneTutorServices;
-        this.toneTutorServices.authenticate(this.toneTutorServices.getRandomSyllable);
+        this.toneTutorServices.authenticate(this.toneTutorServices.getRandomSyllable());
         this.bindEvents();
     },
     // Bind Event Listeners
@@ -277,6 +315,9 @@ var app = {
         console.log('Received Event: ' + id);
     },
     convertMediaFileToBlob: function(mf, success, failure) {
+        // mf is a MediaFile instance, success(blob, attemptMd5) is called after conversion.
+        // Actually converts to an ArrayBuffer rather than a Blob.
+        var app = this;
         if (failure == undefined) {
             failure = function() {console.log('convertMediaFileToBlob failed')};
         }
@@ -289,7 +330,7 @@ var app = {
                 reader.onloadend = function(e) {
                     var attemptMd5 = SparkMD5.ArrayBuffer.hash(this.result);
                     var blob = new Blob([this.result]);
-                    success(blob, attemptMd5);
+                    app.toneTutorServices.ensureAuthenticated(success(blob, attemptMd5));
                 }
 
                 reader.readAsArrayBuffer(file);
@@ -299,7 +340,7 @@ var app = {
             console.log('resolveLocalFileSystemURL failed');
             failure();
         }
-        
+
         window.resolveLocalFileSystemURL(
             mf.fullPath, gotFile, failFile
         );        
@@ -314,8 +355,6 @@ var app = {
                 console.log('captured');
                 var mf = mediaFiles[0];
                 console.log(mf);
-                app.toneTutorServices.resultHandler.setAudioUrl(mf.localURL);
-//                app.toneTutorServices.resultHandler.setAudioUrl(mf.fullPath);
                 app.convertMediaFileToBlob(mf, app.toneTutorServices.checkSyllable);
             },
             function(error){
